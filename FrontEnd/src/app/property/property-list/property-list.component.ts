41275844bf35f6f7dcdd4278d3296b1cf8009f07
vartika <vartikashardul.gupta@gmail.com>
2021-05-25 15:47:04 -0400

    Display a property list

--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -6,7 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-
+  Properties : any = [
+    {
+      "Id": 1,
+      "Type": "House",
+      "Name": "Birla House",
+      "Price": 12000
+    },
+    {
+      "Id": 2,
+      "Type": "Villa",
+      "Name": "Erose House",
+      "Price": 15000
+    },
+    {
+      "Id": 3,
+      "Type": "Mansion",
+      "Name": "Singh Mansion",
+      "Price": 25000
+    },
+    {
+      "Id": 4,
+      "Type": "House",
+      "Name": "Mittal House",
+      "Price": 20000
+    },
+    {
+      "Id": 5,
+      "Type": "Villa",
+      "Name": "Gupta Villa",
+      "Price": 22000
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
