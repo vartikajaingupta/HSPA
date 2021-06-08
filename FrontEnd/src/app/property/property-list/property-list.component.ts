f7fa0e4f7dd36731d7c3a08cbf3f8273d6b75aba
vartika <vartikashardul.gupta@gmail.com>
2021-06-08 12:13:39 -0400

    Add service to pull data from a json file

--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+
 
 @Component({
   selector: 'app-property-list',
@@ -6,41 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  Properties : any = [
-    {
-      "Id": 1,
-      "Type": "House",
-      "Name": "Birla House",
-      "Price": 12000
-    },
-    {
-      "Id": 2,
-      "Type": "Villa",
-      "Name": "Erose House",
-      "Price": 15000
-    },
-    {
-      "Id": 3,
-      "Type": "Mansion",
-      "Name": "Singh Mansion",
-      "Price": 25000
-    },
-    {
-      "Id": 4,
-      "Type": "House",
-      "Name": "Mittal House",
-      "Price": 20000
-    },
-    {
-      "Id": 5,
-      "Type": "Villa",
-      "Name": "Gupta Villa",
-      "Price": 22000
-    },
-  ]
-  constructor() { }
+  properties : any;
+
+  constructor(private HousingService : HousingService) { }
 
   ngOnInit(): void {
+    this.HousingService.getAllProperties().subscribe(
+      data => {
+        this.properties = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    )
   }
-
 }
