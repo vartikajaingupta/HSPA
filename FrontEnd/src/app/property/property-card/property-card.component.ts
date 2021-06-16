5173a737e3c57b5d6391a90d0072bf3477c6b812
vartika <vartikashardul.gupta@gmail.com>
2021-06-16 10:48:25 -0400

    Create type IProperty and make the code typesafe

--- FrontEnd/src/app/property/property-card/property-card.component.ts
+++ FrontEnd/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from "@angular/core";
+import { IProperty } from "../iproperty";
 
 @Component({
   selector: 'app-property-card',
@@ -6,5 +7,5 @@ import { Component, Input } from "@angular/core";
   styleUrls: ['property-card.component.css'],
 })
 export class PropertyCardComponent{
-  @Input() Property : any
+  @Input() Property : IProperty
 };
--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../iproperty';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { HousingService } from 'src/app/services/housing.service';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  properties : any;
+  properties : Array<IProperty>;
 
   constructor(private HousingService : HousingService) { }
 
